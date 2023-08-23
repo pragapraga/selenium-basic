adbd434512a63394894959015fa9821bc767d70a
pragapraga <pragadees24@gmail.com>
2023-08-23 23:53:46 +0530

    Committing Homework

--- src/test/java/com/selenium/day6/W3CAssignment.java
+++ src/test/java/com/selenium/day6/W3CAssignment.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,5 +27,10 @@ public class W3CAssignment {
     driver.findElement(By.xpath("//button[contains(text(),'Try')]")).click();
     driver.switchTo().alert().accept();
     Assert.assertEquals(driver.findElement(By.xpath("//p[@id='demo']")).getText(),"You pressed OK!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
